6f69156d42bc11fcab5319c80d23dd2527ac5d49
adrianasirbu92 <56941428+adrianasirbu92@users.noreply.github.com>
2019-10-25 16:55:46 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
-import javax.sound.midi.Soundbank;
-import java.sql.SQLOutput;
-
 public class Main {
 
     public static void main(String[] args) {
         // 1.Create a loop that will print numbers from 1 to 10, except number 7
-        for (int N = 1; N <= 10; N++) {
-            if (N == 7) continue;
-            System.out.println(N);
+        for (int i = 1; i <= 10; i++) {
+            if (i == 7) continue;
+            System.out.println(i);
         }
         //2.Create An Array of integers from 1 to 5, Call an integer on array using index and print it.
-        int[] array_of_ints = {1, 2, 3, 4, 5};
-        System.out.println(array_of_ints[2]);
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers[2]);
 
         //3.Call index outside of the bound. Handle this exception printing “Outside of bound message.”
         try{
-            int[] Y = {1,2,3,4,5,6,7};
-            System.out.println(Y [8]);
+            int[] y = {1,2,3,4,5,6,7};
+            System.out.println(y [8]);
         }catch (Exception e) {
             System.out.println("Outside of bound message!");
         }
